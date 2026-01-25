6f3b10e402993031d65f8c04b5a9086024808079
Pratik Awasarmol <pratikawasarmol01@gmail.com>
2026-01-25 15:18:29 +0530

    changes in allow origins.

--- app/main.py
+++ app/main.py
@@ -25,17 +25,16 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:8501",
-        "http://127.0.0.1:8501",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://*.vercel.app",
-        "https://secorra-platform.vercel.app",
-    ],
+    "http://localhost:8501",
+    "http://127.0.0.1:8501",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://secorra-platform.vercel.app",
+    "https://secoraa-platform-production.up.railway.app",  # ADD THIS
+    ]
+    ,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
